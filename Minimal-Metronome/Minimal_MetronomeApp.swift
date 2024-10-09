74c5ee1087cb91a4d63b47f54a3ceebda392b64e
hhhello <hhhello0507@dgsw.hs.kr>
2024-10-09 21:12:42 +0900

    Initial Commit

---
+++ Minimal-Metronome/Minimal_MetronomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Minimal_MetronomeApp.swift
+//  Minimal-Metronome
+//
+//  Created by hhhello0507 on 10/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Minimal_MetronomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
