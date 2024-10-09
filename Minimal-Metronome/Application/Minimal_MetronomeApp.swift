1c12ae0dd574503a09ad1fb2a61843c2bb34ccad
hhhello <hhhello0507@dgsw.hs.kr>
2024-10-09 22:10:36 +0900

    Prototype

--- Minimal-Metronome/Application/Minimal_MetronomeApp.swift
+++ Minimal-Metronome/Application/Minimal_MetronomeApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct Minimal_MetronomeApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationStack {
+                ContentView()
+            }
         }
     }
 }
--- Minimal-Metronome/ContentView.swift
+++ Minimal-Metronome/ContentView.swift
@@ -1,24 +1,68 @@
-//
-//  ContentView.swift
-//  Minimal-Metronome
-//
-//  Created by hhhello0507 on 10/9/24.
-//
-
 import SwiftUI
+import Combine
+import AVFoundation
+import Sliders
+
+let url = Bundle.main.url(forResource: "metronome", withExtension: "mp3")
 
 struct ContentView: View {
+    @State private var bpm: Double = 120
+    @State private var timer: Publishers.Autoconnect<Timer.TimerPublisher>?
+    @State private var isPlaying: Bool = false
+    
+    @State private var players: [AVAudioPlayer] = []
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("\(Int(bpm))")
+                .font(.system(size: 108))
+            +
+            Text("BPM")
+                .font(.title)
         }
-        .padding()
+        .safeAreaInset(edge: .bottom) {
+            Slider(value: $bpm, in: 1...360, step: 1, label: {}) { isEditting in
+                print(isEditting)
+                guard !isEditting else { return }
+                setTimer()
+                isPlaying = true
+            }
+            .padding()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .onReceive(timer ?? Timer.publish(every: 1, on: .main, in: .common).autoconnect()) { _ in
+            guard isPlaying,
+                  let url,
+                  let player = try? AVAudioPlayer(contentsOf: url) else {
+                print(isPlaying)
+                return
+            }
+            players.append(player)
+            
+            player.prepareToPlay()
+            player.play()
+        }
+        .background {
+            Color.white
+                .onTapGesture {
+                    setTimer()
+                    isPlaying.toggle()
+                    UIImpactFeedbackGenerator(style: .heavy).impactOccurred()
+                    
+                    if isPlaying {}
+                    else {
+                        stopTimer()
+                    }
+                }
+        }
     }
-}
-
-#Preview {
-    ContentView()
+    
+    private func setTimer() {
+        let interval = 60.0 / bpm
+        timer = Timer.publish(every: interval, on: .main, in: .common).autoconnect()
+    }
+    
+    private func stopTimer() {
+        players = []
+    }
 }
